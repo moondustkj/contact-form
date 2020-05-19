404e432573c6dd96e2aa751f329f3a0d72c05ab1
komalJituri88 <komaljituri88@gmail.com>
2020-05-19 15:46:02 +0530

    Updates

---
+++ precache-manifest.a1c54b376d59c577a757e8f23494b0cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d8efb390706ab4060d6511f4157767",
+    "url": "/contact-form/index.html"
+  },
+  {
+    "revision": "dcc25a3336fc42edb51a",
+    "url": "/contact-form/static/css/2.28eda669.chunk.css"
+  },
+  {
+    "revision": "14e5dae75f16e30e9b4f",
+    "url": "/contact-form/static/css/main.c2c8a515.chunk.css"
+  },
+  {
+    "revision": "dcc25a3336fc42edb51a",
+    "url": "/contact-form/static/js/2.2c25c2fd.chunk.js"
+  },
+  {
+    "revision": "14e5dae75f16e30e9b4f",
+    "url": "/contact-form/static/js/main.eb8f24bd.chunk.js"
+  },
+  {
+    "revision": "f5c2949b151b6e313283",
+    "url": "/contact-form/static/js/runtime~main.2f609b49.js"
+  },
+  {
+    "revision": "4ab8de7af0afb244374db6154e300a14",
+    "url": "/contact-form/static/media/newyorker cover.4ab8de7a.jpg"
+  }
+]);
